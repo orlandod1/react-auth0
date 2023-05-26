e3487c78e43cf207691ef24c8c78cbcb8678cb4a
orlandod1 <orlandoldiaz@uniguajira.edu.co>
2023-05-26 18:17:57 -0500

    Se agrega el proyecto

---
+++ react-auth0/src/App.js
@@ -0,0 +1,30 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+import { LoginButton } from "./Login";
+import { LogoutButton } from "./Logout";
+import { Profile } from "./Profile";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const { isAuthenticated } = useAuth0();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1> LOGIN USANDO AUTH0 POR ORLANDO DIAZ</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        {isAuthenticated ? (
+          <>
+            <Profile />
+            <LogoutButton />
+          </>
+        ) : (
+          <LoginButton />
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
